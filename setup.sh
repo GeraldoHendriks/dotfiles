f299e3d30402f9ccb315f25960448c564cbdf85b
Geraldo Hendriks <geraldohendriks@gmail.com>
2022-05-22 13:11:17 +0200

    feat: made Makefile into shell script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+
+rm -rf ~/.config/qtile ~/.config/dunst ~/.config/picom ~/.config/alacritty ~/.config/wallpapers ~/.config/rofi
+mkdir ~/.config ~/.config/wallpapers ~/.config/qtile ~/.config/dunst ~/.config/picom ~/.config/alacritty ~/.config/rofi
+cp ./wallpaper.png ~/.config/wallpapers/
